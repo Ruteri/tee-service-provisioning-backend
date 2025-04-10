bb07f939fac049308328ac6799485b3889679223
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2025-04-10 13:21:48 +0200

    Refactors registry and metadata client cmd into one

--- cmd/registry_client/main.go
+++ cmd/registry_client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -11,27 +12,29 @@ import (
 	"github.com/ruteri/tee-service-provisioning-backend/cryptoutils"
 	"github.com/ruteri/tee-service-provisioning-backend/interfaces"
 	"github.com/urfave/cli/v2"
+
+	tdx_abi "github.com/google/go-tdx-guest/abi"
+	tdx_pb "github.com/google/go-tdx-guest/proto/tdx"
+	"github.com/google/go-tdx-guest/verify"
 )
 
-var flags []cli.Flag = []cli.Flag{
-	&cli.StringFlag{
-		Name:  "provisioning-server-addr",
-		Value: "http://127.0.0.1:8080",
-		Usage: "Provisioning server address to request",
-	},
-	&cli.StringFlag{
-		Name:     "app-contract",
-		Required: true,
-		Usage:    "Application governance contract address to request provisioning for. 40-char hex string with no 0x prefix",
-	},
-	&cli.StringFlag{
-		Name:  "debug-set-attestation-type-header",
-		Usage: "If provided the provisioner will set the attestation type header",
-	},
-	&cli.StringFlag{
-		Name:  "debug-set-attestation-measurement-header",
-		Usage: "If provided the provisioner will set the attestation measurement header",
-	},
+var flagServerAddr *cli.StringFlag = &cli.StringFlag{
+	Name:  "provisioning-server-addr",
+	Value: "http://127.0.0.1:8080",
+	Usage: "Provisioning server address to request",
+}
+var flagAppAddr *cli.StringFlag = &cli.StringFlag{
+	Name:     "app-contract",
+	Required: true,
+	Usage:    "Application governance contract address to request provisioning for. 40-char hex string with no 0x prefix",
+}
+var flagAttsetationType *cli.StringFlag = &cli.StringFlag{
+	Name:  "debug-set-attestation-type-header",
+	Usage: "If provided the provisioner will set the attestation type header",
+}
+var flagAttsetationMeasurement *cli.StringFlag = &cli.StringFlag{
+	Name:  "debug-set-attestation-measurement-header",
+	Usage: "If provided the provisioner will set the attestation measurement header",
 }
 
 const usage string = ``
@@ -40,14 +43,43 @@ func main() {
 	app := &cli.App{
 		Name:  "registry client",
 		Usage: usage,
-		Flags: flags,
-		Action: func(cCtx *cli.Context) error {
-			provisioner, err := NewProvisioner(cCtx)
-			if err != nil {
-				return err
-			}
-
-			return provisioner.Do()
+		Flags: []cli.Flag{
+			flagAppAddr,
+			flagServerAddr,
+		},
+		Commands: []*cli.Command{
+			&cli.Command{
+				Name:        "register",
+				Usage:       "",
+				Description: "",
+				Flags: []cli.Flag{
+					flagAttsetationType,
+					flagAttsetationMeasurement,
+				},
+				Action: func(cCtx *cli.Context) error {
+					c, err := NewClientConfig(cCtx)
+					if err != nil {
+						return err
+					}
+					return c.Register()
+				},
+			},
+			&cli.Command{
+				Name:        "metadata",
+				Usage:       "",
+				Description: "",
+				Flags: []cli.Flag{
+					flagAttsetationType,
+					flagAttsetationMeasurement,
+				},
+				Action: func(cCtx *cli.Context) error {
+					c, err := NewClientConfig(cCtx)
+					if err != nil {
+						return err
+					}
+					return c.GetAppMetadata()
+				},
+			},
 		},
 	}
 
@@ -56,40 +88,94 @@ func main() {
 	}
 }
 
-type Provisioner struct {
-	AppContract          interfaces.ContractAddress
-	RegistrationProvider api.RegistrationProvider
+type Client struct {
+	AppContract interfaces.ContractAddress
+	Provider    interface {
+		api.MetadataProvider
+		api.RegistrationProvider
+	}
 }
 
-func NewProvisioner(cCtx *cli.Context) (*Provisioner, error) {
-	appContract, err := interfaces.NewContractAddressFromHex(cCtx.String("app-contract"))
+func NewClientConfig(cCtx *cli.Context) (*Client, error) {
+	appContract, err := interfaces.NewContractAddressFromHex(cCtx.String(flagAppAddr.Name))
 	if err != nil {
 		return nil, fmt.Errorf("could not parse app contract address: %w", err)
 	}
 
 	registrationProvider := &clients.ProvisioningClient{
-		ServerAddr:                cCtx.String("provisioning-server-addr"),
-		SetAttestationType:        cCtx.String("debug-set-attestation-type-header"),
-		SetAttestationMeasurement: cCtx.String("debug-set-attestation-measurement-header"),
+		ServerAddr:                cCtx.String(flagServerAddr.Name),
+		SetAttestationType:        cCtx.String(flagAttsetationType.Name),
+		SetAttestationMeasurement: cCtx.String(flagAttsetationType.Name),
 	}
 
-	return &Provisioner{
-		AppContract:          appContract,
-		RegistrationProvider: registrationProvider,
+	return &Client{
+		AppContract: appContract,
+		Provider:    registrationProvider,
 	}, nil
 }
 
-func (p *Provisioner) Do() error {
-	_, csr, err := cryptoutils.CreateCSRWithRandomKey(string(interfaces.NewAppCommonName(p.AppContract)))
+func (c *Client) Register() error {
+	_, csr, err := cryptoutils.CreateCSRWithRandomKey(string(interfaces.NewAppCommonName(c.AppContract)))
 	if err != nil {
 		return fmt.Errorf("could not create instance CSR: %w", err)
 	}
 
-	parsedResponse, err := p.RegistrationProvider.Register(p.AppContract, csr)
+	parsedResponse, err := c.Provider.Register(c.AppContract, csr)
 	if err != nil {
 		return fmt.Errorf("registration failed: %w", err)
 	}
 	encodedRegistrationResp, _ := json.Marshal(parsedResponse)
 	fmt.Println(string(encodedRegistrationResp))
+	return nil
+}
+
+func (c *Client) GetAppMetadata() error {
+	parsedResponse, err := c.Provider.GetAppMetadata(c.AppContract)
+	if err != nil {
+		return fmt.Errorf("metadata request failed: %w", err)
+	}
+	encodedResp, _ := json.Marshal(parsedResponse)
+	fmt.Println(string(encodedResp))
+
+	err = VerifyDCAPAttestation(c.AppContract, parsedResponse)
+	if err != nil {
+		return fmt.Errorf("metadata attestation verification failed: %w", err)
+	}
+
+	return nil
+}
+
+func VerifyDCAPAttestation(contractAddr interfaces.ContractAddress, resp *api.MetadataResponse) error {
+	protoQuote, err := tdx_abi.QuoteToProto(resp.Attestation)
+	if err != nil {
+		return fmt.Errorf("could not parse quote: %w", err)
+	}
+
+	v4Quote, err := func() (*tdx_pb.QuoteV4, error) {
+		switch q := protoQuote.(type) {
+		case *tdx_pb.QuoteV4:
+			return q, nil
+		default:
+			return nil, fmt.Errorf("unsupported quote type: %T", q)
+		}
+	}()
+	if err != nil {
+		return err
+	}
+
+	options := verify.DefaultOptions()
+	// TODO: fetch collateral before verifying to distinguish the error better
+	err = verify.TdxQuote(protoQuote, options)
+	if err != nil {
+		return fmt.Errorf("quote verification failed: %w", err)
+	}
+
+	expectedReportData := api.ReportData(contractAddr, resp.CACert, resp.AppPubkey)
+	if !bytes.Equal(v4Quote.TdQuoteBody.ReportData, expectedReportData[:]) {
+		return fmt.Errorf("invalid report data %x, expected %x", v4Quote.TdQuoteBody.ReportData, expectedReportData[:])
+	}
+
+	fmt.Println("attestation validation successful")
+
 	return nil
 }
